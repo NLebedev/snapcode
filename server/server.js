7d2e11dd66390e4f28c5683ae81d3908027845f2
nlebedev <nlebedev100@gmail.com>
2016-05-24 00:00:59 -0700

    server port fix for production

--- server/server.js
+++ server/server.js
@@ -8,7 +8,8 @@ require('./config/middleware.js')(app, express);
 require('./config/routes.js')(app, express);
 
 // start listening to requests on port 8000
-app.listen(8000);
+var port = process.env.PORT || 8000;
+app.listen(port);
 
 // export our app for testing and flexibility, required by index.js
 module.exports = app;
